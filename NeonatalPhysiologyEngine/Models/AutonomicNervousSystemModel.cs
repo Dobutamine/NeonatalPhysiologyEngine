3a9bf63c5e357829d9ce1ed49c7d07b7f478752d
Timothy Antonius <tim.antonius@outlook.com>
2020-01-21 21:53:13 +0100

    acidbase

--- NeonatalPhysiologyEngine/Models/AutonomicNervousSystemModel.cs
+++ NeonatalPhysiologyEngine/Models/AutonomicNervousSystemModel.cs
@@ -8,15 +8,27 @@ namespace NeonatalPhysiologyEngine.Models
     {
         Model currentModel;
 
+        BloodCompartment AA;
+
         public AutonomicNervousSystemModel(Model cm)
         {
             currentModel = cm;
 
             currentModel.modelInterface.StatusMessage = $"Initialized the autonomic nervous system model.";
+
+            // find a reference to the heart chamber compartments
+            foreach (BloodCompartment bloodComp in currentModel.modelDefinition.blood_compartments)
+            {
+                if (bloodComp.name == "AA")
+                {
+                    AA = bloodComp;
+                }
+            }
         }
 
         public void ModelCycle()
         {
+            currentModel.acidbase.CalcAcidBaseFromTCO2(AA);
 
         }
     }
